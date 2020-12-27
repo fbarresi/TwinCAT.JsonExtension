01ba696c87b90c6bf2f34bb96f8a7105e8952083
Federico Barresi <federico.barresi@evopro-ag.de>
2020-12-27 21:33:41 +0100

    upgraded nuget packages for tests
    - removed unused dependencies

--- TwinCAT.JsonExtension.Tests/TestReadWriteOperations.cs
+++ TwinCAT.JsonExtension.Tests/TestReadWriteOperations.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Moq;
-using NUnit.Framework;
 using Shouldly;
 using TwinCAT.Ads;
 using TwinCAT.TypeSystem;
