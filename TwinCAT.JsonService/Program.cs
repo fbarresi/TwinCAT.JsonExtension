ad07c13dca3c4c0b8978b1076f5d75aee441dd1b
Federico Barresi <federico.barresi@ams-osram.com>
2022-06-11 21:45:13 +0200

    added api information

--- TwinCAT.JsonService/Program.cs
+++ TwinCAT.JsonService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "TwinCAT Json Service",
+        Description = "A Web API for with json format for read/write operations with TwinCAT",
+        TermsOfService = new Uri("https://github.com/fbarresi/TwinCAT.JsonExtension"),
+        Contact = new OpenApiContact
+        {
+            Name = "Federico Barresi",
+            Url = new Uri("https://github.com/fbarresi")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://github.com/fbarresi/TwinCAT.JsonExtension/blob/master/LICENSE")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +33,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
