c89fa3ff5fa5504a94e474d246a7557f9dabe02e
Federico Barresi <federico.barresi@ams-osram.com>
2023-04-18 22:34:50 +0200

    enhanced VariablesController
    - added more logging
    - added time measurement

--- TwinCAT.JsonService/Controllers/VariablesController.cs
+++ TwinCAT.JsonService/Controllers/VariablesController.cs
@@ -23,22 +23,32 @@ public class VariablesController : ControllerBase
     [Route("{name}")]
     public async Task<object> Get(string name, [FromQuery]bool enumsToString = false)
     {
-        return await clientService.Client.ReadJson(name, force: true, stringifyEnums: enumsToString);
+        logger.LogInformation("Trying to get {variable} (enumsToString={enumsToString})...", name, enumsToString);
+        var start = DateTime.Now;
+        var json = await clientService.Client.ReadJson(name, force: true, stringifyEnums: enumsToString);
+        logger.LogInformation("Var {variable} read in {readTime}", name, DateTime.Now-start);
+        return json;
     }
     
     [HttpPost]
     [Route("{name}")]
     public async Task Set(string name, object value)
     {
+        logger.LogInformation("Trying to set {variable}...", name);
+        var start = DateTime.Now;
         var json = new JObject(value);
         await clientService.Client.WriteJson(name, json, force: true);
+        logger.LogInformation("Var {variable} set in {readTime}", name, DateTime.Now-start);
     }
     
     [HttpPost]
     [Route("list/{name}")]
     public async Task Set(string name, IEnumerable<object> value)
     {
+        logger.LogInformation("Trying to set list {variable}...", name);
+        var start = DateTime.Now;
         var json = new JArray(value);
         await clientService.Client.WriteJson(name, json, force: true);
+        logger.LogInformation("list {variable} set in {readTime}", name, DateTime.Now-start);
     }
 }
