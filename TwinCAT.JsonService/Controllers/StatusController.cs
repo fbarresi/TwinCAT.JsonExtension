7c463755635b93927ee0176749c2c5d4fd58d17b
Federico Barresi <federico.barresi@ams-osram.com>
2023-05-16 22:02:12 +0200

    introduced status controller

---
+++ TwinCAT.JsonService/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TwinCAT.JsonService.Interfaces;
+
+namespace TwinCAT.JsonService.Controllers;
+
+public class StatusController : ControllerBase
+{
+    private readonly ILogger<StatusController> logger;
+    private readonly IClientService clientService;
+
+    public StatusController(ILogger<StatusController> logger, IClientService clientService)
+    {
+        this.logger = logger;
+        this.clientService = clientService;
+    }
+    [HttpGet]
+    [Route("connection")]
+    public Task<ConnectionState> GetConnectionState()
+    {
+        return clientService.ConnectionState.FirstAsync().ToTask();
+    }
+    [HttpGet]
+    [Route("ads")]
+    public Task<string> GetAdsState()
+    {
+        return clientService.AdsState.FirstAsync().ToTask();
+    }
+}
