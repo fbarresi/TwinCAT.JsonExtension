98867db0e7dd0e0d5f3fe08875bd3718bd958538
Federico Barresi <federico.barresi@ams-osram.com>
2023-07-15 21:38:48 +0200

    added controller for symbols

---
+++ TwinCAT.JsonService/Controllers/SymbolsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using TwinCAT.JsonExtension;
+using TwinCAT.JsonService.Interfaces;
+using TwinCAT.TypeSystem;
+
+namespace TwinCAT.JsonService.Controllers;
+
+public class SymbolsController : ControllerBase
+{
+    private readonly ILogger<SymbolsController> logger;
+    private readonly IClientService clientService;
+
+    public SymbolsController(ILogger<SymbolsController> logger, IClientService clientService)
+    {
+        this.logger = logger;
+        this.clientService = clientService;
+    }
+
+    [HttpGet]
+    [Route("tree")]
+    public Task<ISymbolCollection<ISymbol>> GetTree()
+    {
+        return Task.FromResult(clientService.TreeViewSymbols);
+    }
+    
+    [HttpPost]
+    [Route("flat")]
+    public Task<ISymbolCollection<ISymbol>> GetFlat()
+    {
+        return Task.FromResult(clientService.FlatViewSymbols);
+    }
+    
+    [HttpPost]
+    [Route("get/{path}")]
+    public Task<IEnumerable<ISymbol>> StartingWith(string path)
+    {
+        return Task.FromResult(clientService.FlatViewSymbols.Where(s => s.InstancePath.StartsWith(path)));
+    }
+    
+    [HttpPost]
+    [Route("query/{path}")]
+    public Task<IEnumerable<ISymbol>> query(string name)
+    {
+        return Task.FromResult(clientService.FlatViewSymbols.Where(s => s.InstancePath.Contains(name)));
+    }
+}
